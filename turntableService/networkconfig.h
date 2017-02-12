e89a0f6710bbbb80401f768093ef26fb2fac1a05
Thomas Prioul <thomas.prioul@outlook.com>
2017-02-12 18:17:37 +0100

    Add network protocol description

--- turntableService/networkconfig.h
+++ turntableService/networkconfig.h
@@ -9,17 +9,19 @@ namespace NetworkConfig  {
     const quint16 defaultPort = 2017;
     const int32_t defaultSteps = 19200;
 
+    // All commands end up with \n, \r is not mandatory
+
     namespace Query {
         const std::string reset = "RR";
         const std::string stop = "RS";
-        const std::string move = "RD";
-        const std::string moveToTrack = "RD track";
-        const std::string moveToPosition = "RD pos";
-        const std::string position = "RP";
-        const std::string sendConfig = "RC";
-        const std::string nbSteps = "RI steps";
-        const std::string addTrack = "RV add"; // RV add <trackName> <pos>
-        const std::string deleteTrack = "RV del"; // RV del <trackName>
+        const std::string move = "RD"; // RD <pos>
+        const std::string moveToTrack = "RD track"; // RD track "<trackname>"
+        const std::string moveToPosition = "RD pos"; // RD pos <pos>
+        const std::string position = "RP"; // RD pos
+        const std::string sendConfig = "RC"; // RC
+        const std::string nbSteps = "RI steps"; // RI steps
+        const std::string addTrack = "RV add"; // RV add "<trackName>" <pos>
+        const std::string deleteTrack = "RV del"; // RV del "<trackName>"
     }
 
     namespace Notification {
@@ -27,13 +29,13 @@ namespace NetworkConfig  {
         const std::string moveStopped = "SD stop";
         const std::string resetStarted = "SR start";
         const std::string resetStopped = "SR stop "; // SR stop <success: 0 or 1>
-        const std::string position = "SP ";
-        const std::string beginSendConfig = "SC begin";
-        const std::string endSendConfig = "SC end";
-        const std::string nbSteps = "SI steps";
-        const std::string trackDefinition = "SV ";
-        const std::string addTrack = "SV add ";
-        const std::string deleteTrack = "SV del ";
+        const std::string position = "SP "; // SP <pos>
+        const std::string beginSendConfig = "SC begin"; // SC begin
+        const std::string endSendConfig = "SC end"; // SC end
+        const std::string nbSteps = "SI steps"; // SI steps <nbSteps>
+        const std::string trackDefinition = "SV "; // SV "<trackname>" <pos>
+        const std::string addTrack = "SV add "; // SV add "<trackname>" <pos>
+        const std::string deleteTrack = "SV del "; // SV del "<trackname>" <pos>
     }
 }
 
